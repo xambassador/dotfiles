c86e2eb14e502a6ce8794657461173c580ee4085
Yash Ramoliya <yashramoliya12@gmail.com>
2026-06-09 21:44:29 +0530

    feat: update nvtree filters and layout

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -70,18 +70,56 @@ return {
   {
     "nvim-tree/nvim-tree.lua",
     opts = {
+      filters = {
+        custom = {
+          "^.git$",
+          "^node_modules$",
+          "^dist$",
+          "^build$",
+          "^vendor$",
+          "^public$",
+          "^coverage$",
+          "^target$",
+          "^out$",
+          "^logs$",
+          "^tmp$",
+          "^.next$",
+        },
+      },
+      git = {
+        ignore = true,
+      },
       view = {
         -- side = "right",
         float = {
           enable = true,
-          open_win_config = {
-            relative = "editor",
-            border = "rounded",
-            width = 30,
-            height = 30,
-            row = 1,
-            col = 1,
-          },
+          -- open_win_config = {
+          --   relative = "editor",
+          --   border = "rounded",
+          --   width = 30,
+          --   height = 30,
+          --   row = 1,
+          --   col = 1,
+          -- },
+          open_win_config = function()
+            local screen_w = vim.opt.columns:get()
+            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+
+            local window_w = math.floor(screen_w * 0.4)
+            local window_h = math.floor(screen_h * 0.8)
+
+            local center_x = (screen_w - window_w) / 2
+            local center_y = ((screen_h - window_h) / 2) - vim.opt.cmdheight:get()
+
+            return {
+              border = "rounded",
+              relative = "editor",
+              row = center_y,
+              col = center_x,
+              width = window_w,
+              height = window_h,
+            }
+          end,
         },
       },
     },
