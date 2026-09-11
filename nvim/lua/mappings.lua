8388008f605c3af1d755cc04b199b28802239f71
Yash Ramoliya <yashramoliya12@gmail.com>
2026-09-11 21:35:42 +0530

    feat: add keybindings for code action

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -5,7 +5,6 @@ local map = vim.keymap.set
 map("i", "jk", "<ESC>")
 map("i", "<C-l>", 'copilot#Accept("<CR>")', { expr = true, replace_keycodes = false })
 
-
 -- <leader>gt (git_status)
 -- <leader>cm (git_commits)
 map("n", "<leader>gb", "<cmd>Telescope git_branches<CR>", { desc = "telescope git branches" })
@@ -17,3 +16,23 @@ map("n", "<leader>fp", function()
   -- vim.fn.setreg("+", path)
   vim.notify(path)
 end, { desc = "show full path of current buffer" })
+
+map("n", "<leader>df", function()
+  vim.diagnostic.open_float(nil, {
+    border = "rounded",
+    focusable = true,
+    scope = "cursor",
+    source = "if_many",
+  })
+end, { desc = "Floating diagnostics" })
+
+map("n", "<leader>dF", function()
+  vim.diagnostic.open_float(nil, {
+    border = "rounded",
+    focusable = true,
+    scope = "buffer",
+    source = "if_many",
+  })
+end, { desc = "Buffer diagnostics float" })
+
+map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
